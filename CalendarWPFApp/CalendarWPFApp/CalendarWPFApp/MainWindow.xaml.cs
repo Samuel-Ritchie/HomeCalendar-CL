64a78985b3120306fc964811a76c95b4d3223099
CompSciZach <2273406@johnabbottcollege.net>
2024-04-17 12:20:46 -0400

    Finished backend functionality for main window

--- CalendarWPFApp/CalendarWPFApp/CalendarWPFApp/MainWindow.xaml.cs
+++ CalendarWPFApp/CalendarWPFApp/CalendarWPFApp/MainWindow.xaml.cs
@@ -29,6 +29,8 @@ namespace CalendarWPFApp
             fileDialog.Multiselect = false; //cant select many files
             bool? choseFile = fileDialog.ShowDialog();
 
+            
+
             if(choseFile == true)
             {
                 //user picked a file
@@ -36,17 +38,54 @@ namespace CalendarWPFApp
                 string fileName = fileDialog.SafeFileName;
                 chosenFileName.Text = fileName;
                 chosenDirectoryName.Text = fullPath;
+
             } else
             {
-                //did not pick a file
+                chosenFileName.Text = "Please select a valid file.";
             }
         }
 
-        private void openEventCreationPage_Click(object sender, RoutedEventArgs e)
+        private void OpenWindow_Click(object sender, RoutedEventArgs e)
         {
-            createPromptWindow secondWindow = new createPromptWindow();
-            this.Visibility = Visibility.Hidden;
-            secondWindow.Show();
+
+
+            //validate selected file before finding?
+            if (chosenFileName.Text == "Please select a valid file.")
+            {
+                ErrorFind.Text = "Please select a valid file before searching.";
+            }else
+            {
+                //prompt next window
+                
+            }
+
+            
+
         }
+
+        private void swapBtnState_Click(object sender, RoutedEventArgs e)
+        {
+            string createCalendar = "Create calendar";
+            string findCalendar = "Find calendar";
+       
+                if (FindBtn.Content.ToString() == "Find" || FindBtn.Content.ToString() == findCalendar)
+                {
+                    FindBtn.Content = createCalendar;
+
+                }
+                else if (FindBtn.Content.ToString() == createCalendar)
+                {
+                    FindBtn.Content = findCalendar;
+                }
+           
+        }
+
+        //method for opening Sam's window (window name not up to date)
+        //private void openEventCreationPage_Click(object sender, RoutedEventArgs e)
+        //{
+        //    createPromptWindow secondWindow = new createPromptWindow();
+        //    this.Visibility = Visibility.Hidden;
+        //    secondWindow.Show();
+        //}
     }
 }
